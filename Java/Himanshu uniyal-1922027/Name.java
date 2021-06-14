98c7d54dd639854124c665bca48c2bf38c00852a
Himmu269 <85777849+Himmu269@users.noreply.github.com>
2021-06-14 17:33:22 +0530

    Add files via upload

---
+++ Java/Himanshu uniyal-1922027/Name.java
@@ -0,0 +1,14 @@
+class Name
+{
+static String name="HIMANSHU";
+static int rollno=1922027;
+static void display()
+{
+System.out.println("Name : "+ name);
+System.out.println("Rollno: "+ rollno);
+}
+public static void main(String args[] )
+{
+display( );
+}
+} 
